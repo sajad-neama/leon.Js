d1bd266ed1d61034df65ceb2c78167495c553143
Sajad Neama Kadhum <sajad.neama.kazem@gmail.com>
2024-12-02 13:37:47 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-const myPar =document.querySelectorAll(".colorpar span");
+const myPar =document.querySelectorAll(".color");
 console.log(window.localStorage.color)
 if (window.localStorage.color != null ){
     document.documentElement.style.setProperty("--main-color" , `${window.localStorage.color}` )
-}
+};
 
 myPar.forEach(function (e) {
     e.addEventListener("click" , function (ele) {
-        window.localStorage.setItem("color", `${e.className}`)
+        window.localStorage.setItem("color", `${e.dataset.color}`);
         document.documentElement.style.setProperty("--main-color" , `${window.localStorage.getItem("color")}` )
-    })
-})
+    });
+});
+
+const myPPar= document.querySelector(".par");
+const myIcon= document.querySelector(".iconn");
+
+myIcon.addEventListener("click" , (e)=>{
+    if (myPPar.classList.contains("evant") ){
+        myPPar.classList.remove("evant")
+        e.currentTarget.classList.remove("evantt")
+    }else{
+        myPPar.classList.add("evant")
+        e.currentTarget.classList.add("evantt")
+    };
+
+});
